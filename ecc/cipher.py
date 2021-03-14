9052c0646a1a60195f8794cf488ecb00f716019c
lc6chang <lc6chang@gmail.com>
2021-03-14 17:39:51 +0800

    Add encrypt_point/decrypt_point method in ElGamal

--- ecc/cipher.py
+++ ecc/cipher.py
@@ -12,27 +12,25 @@ class ElGamal:
 
     def encrypt(self, plaintext: bytes, public_key: Point,
                 randfunc: Callable = None) -> Tuple[Point, Point]:
-        randfunc = randfunc or urandom
-        # Base point
-        G = self.curve.G
-        # Encode plaintext into curve point
+        return self.encrypt_bytes(plaintext, public_key, randfunc)
+
+    def decrypt(self, private_key: int, C1: Point, C2: Point) -> bytes:
+        return self.decrypt_bytes(private_key, C1, C2)
+
+    def encrypt_bytes(self, plaintext: bytes, public_key: Point,
+                      randfunc: Callable = None) -> Tuple[Point, Point]:
+        # Encode plaintext into a curve point
         M = self.curve.encode_point(plaintext)
+        return self.encrypt_point(M, public_key, randfunc)
 
-        random.seed(randfunc(1024))
-        k = random.randint(1, self.curve.n)
-
-        C1 = k * G
-        C2 = M + k * public_key
-        return C1, C2
-
-    def decrypt(self, private_key: int, C1: Point, C2: Point):
+    def decrypt_bytes(self, private_key: int, C1: Point, C2: Point) -> bytes:
         M = C2 + (self.curve.n - private_key) * C1
         return self.curve.decode_point(M)
 
-    def encrypt_raw(self, plaintext: Point, public_key: Point,
-                    randfunc: Callable = None) -> Tuple[Point, Point]:
+    def encrypt_point(self, plaintext: Point, public_key: Point,
+                      randfunc: Callable = None) -> Tuple[Point, Point]:
         randfunc = randfunc or urandom
-        # Base point
+        # Base point G
         G = self.curve.G
         M = plaintext
 
@@ -43,6 +41,6 @@ class ElGamal:
         C2 = M + k * public_key
         return C1, C2
 
-    def decrypt_raw(self, private_key: int, C1: Point, C2: Point):
+    def decrypt_point(self, private_key: int, C1: Point, C2: Point) -> Point:
         M = C2 + (self.curve.n - private_key) * C1
         return M
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_namespace_packages
+
 from ecc import __version__
 
 dependencies = [
-    "dataclasses"
+    "dataclasses",
 ]
 
 setup(
     name="ecc-pycrypto",
-    author="ChangLiu",
+    author="lc6chang",
     author_email="lc6chang@gmail.com",
     version=__version__,
     packages=find_namespace_packages(),
