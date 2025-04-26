2a7cae00e8b6d8aa44194f73197b56af5a73e28d
ChangLiu <lc6chang@gmail.com>
2025-04-26 20:11:05 +0800

    Move curve instances to registery.py (#31)

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -1,32 +1,39 @@
 import unittest
 
-from ecc.curve import (
-    P256, secp256k1, Curve25519, M383, E222, E382, Point
-)
+from ecc import curve
+from ecc import registry
 
-CURVES = [P256, secp256k1, Curve25519, M383, E222, E382]
+
+CURVES: list[curve.Curve] = [
+    registry.P256,
+    registry.secp256k1,
+    registry.Curve25519,
+    registry.M383,
+    registry.E222,
+    registry.E382,
+]
 
 
 class PointAndCurveTestCase(unittest.TestCase):
     def test_operator(self):
-        for curve in CURVES:
-            with self.subTest(curve=curve):
-                P = curve.G
+        for curve_ in CURVES:
+            with self.subTest(name=curve_.name):
+                P = curve_.G
                 self.assertEqual(P + P, 2 * P)
-                self.assertEqual(P - P, curve.INF)
-                self.assertEqual(P + (-P), curve.INF)
+                self.assertEqual(P - P, curve_.INF)
+                self.assertEqual(P + (-P), curve_.INF)
                 self.assertEqual(P + P + P + P + P, P * 5)
                 self.assertEqual(-P - P - P - P - P, -5 * P)
                 self.assertEqual(P - 2 * P, -P)
                 self.assertEqual(20 * P + 4 * P, 10 * P + 14 * P)
-                self.assertEqual(curve.INF + 10 * P, 10 * P)
-                self.assertEqual(curve.INF - 3 * P, -3 * P)
-                self.assertEqual(curve.INF + curve.INF, curve.INF)
-                self.assertEqual(0 * P, curve.INF)
-                self.assertEqual(1000 * curve.INF, curve.INF)
+                self.assertEqual(curve_.INF + 10 * P, 10 * P)
+                self.assertEqual(curve_.INF - 3 * P, -3 * P)
+                self.assertEqual(curve_.INF + curve_.INF, curve_.INF)
+                self.assertEqual(0 * P, curve_.INF)
+                self.assertEqual(1000 * curve_.INF, curve_.INF)
 
     def test_double_points_y_equals_to_0(self):
-        P = Point(x=0, y=0, curve=Curve25519)
-        self.assertEqual(P + P, Curve25519.INF)
-        self.assertEqual(2 * P, Curve25519.INF)
-        self.assertEqual(-2 * P, Curve25519.INF)
+        P = curve.Point(x=0, y=0, curve=registry.Curve25519)
+        self.assertEqual(P + P, registry.Curve25519.INF)
+        self.assertEqual(2 * P, registry.Curve25519.INF)
+        self.assertEqual(-2 * P, registry.Curve25519.INF)
